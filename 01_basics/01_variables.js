db19c77a19a9e8d0118e37b83a0dc16e52ba0cd0
Garg-Anushka <112790216+Garg-Anushka@users.noreply.github.com>
2024-11-06 12:19:28 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+// const value cant be changed
+
+// let and var value can be changed
+
+/* Prefer using let over var because of function scope and block scope */
+
+// while using let if value is not assigned it is undefined
+
+// console.table([ , , ]): prints value in form of table
